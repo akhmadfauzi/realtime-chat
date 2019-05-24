c2b6971bb3695b15ba5fd6fa52ba9cd9fc698294
oji <sifauzi@gmail.com>
2019-05-24 13:38:26 +0700

    Test FCM

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,18 +1,37 @@
-// import * as firebase from 'firebase/app';
-// import 'firebase/firebase-messaging';
-//import { FIREBASE } from '../src/config';
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
-// const firebase = require('firebase');
-// const messaging = require('firebase/firebase-messaging');
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+const FIREBASE_CONFIG = {
+	apiKey: "AIzaSyB4KyRyONOn-cTEHpEgBr8Vrx0pCuvOUh8",
+	authDomain: "amartha-chat-app-test.firebaseapp.com",
+	databaseURL: "https://amartha-chat-app-test.firebaseio.com",
+	projectId: "amartha-chat-app-test",
+	storageBucket: "amartha-chat-app-test.appspot.com",
+	messagingSenderId: "438069375682",
+	appId: "1:438069375682:web:a725f26ad25a6649"
+};
+firebase.initializeApp(FIREBASE_CONFIG);
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
 
 
-
-// firebase.initializeApp({
-//     messagingSenderId: "438069375682"
-// });
-// messaging.setBackgroundMessageHandler((payload => {
-//     console.log(payload);
-// }))
-
-
+messaging.setBackgroundMessageHandler(function(payload) {
+	console.log('[firebase-messaging-sw.js] Received background message ', payload);
+	// Customize notification here
+	var notificationTitle = payload.data.name;
+	var notificationOptions = {
+	  body: payload.data.message,
+	  icon: '/firebase-logo.png'
+	};
+  
+	return self.registration.showNotification(notificationTitle,
+	  notificationOptions);
+  });
 
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const FIREBASE = {
+export const FIREBASE_CONFIG = {
     apiKey: "AIzaSyB4KyRyONOn-cTEHpEgBr8Vrx0pCuvOUh8",
     authDomain: "amartha-chat-app-test.firebaseapp.com",
     databaseURL: "https://amartha-chat-app-test.firebaseio.com",
